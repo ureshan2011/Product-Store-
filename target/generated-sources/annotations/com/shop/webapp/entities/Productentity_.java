a31ce9d09f6e54e58d4c9f762e208ac36d500248
Yasas Sri <ureshan2011@gmail.com>
2017-07-12 23:46:30 +0530

    second commit

---
+++ target/generated-sources/annotations/com/shop/webapp/entities/Productentity_.java
@@ -0,0 +1,16 @@
+package com.shop.webapp.entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-07-12T20:27:59")
+@StaticMetamodel(Productentity.class)
+public class Productentity_ { 
+
+    public static volatile SingularAttribute<Productentity, String> productname;
+    public static volatile SingularAttribute<Productentity, String> productqty;
+    public static volatile SingularAttribute<Productentity, Long> id;
+    public static volatile SingularAttribute<Productentity, String> productprice;
+
+}
